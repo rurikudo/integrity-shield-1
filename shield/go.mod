731c278774a7282616c319c14451b38d7edb5a17
Riko Kudo <rurikudo@ibm.com>
2022-09-12 22:18:47 +0900

    test for using gatekeeper external data
    Signed-off-by: Riko Kudo <rurikudo@ibm.com>

--- shield/go.mod
+++ shield/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/jinzhu/copier v0.3.2
+	github.com/open-policy-agent/frameworks/constraint v0.0.0-20220719222628-b0dbc52e8449
 	github.com/pkg/errors v0.9.1
 	github.com/sigstore/cosign v1.10.1
 	github.com/sigstore/k8s-manifest-sigstore v0.3.1-0.20220810053329-14f7cab4fd52
@@ -12,7 +13,7 @@ require (
 	k8s.io/api v0.25.0-alpha.2
 	k8s.io/apimachinery v0.25.0-alpha.2
 	k8s.io/client-go v0.25.0-alpha.2
-	sigs.k8s.io/controller-runtime v0.12.2
+	sigs.k8s.io/controller-runtime v0.12.3
 )
 
 replace github.com/stolostron/integrity-shield/shield => ./
--- shield/main.go
+++ shield/main.go
@@ -22,8 +22,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"path"
+	"strings"
 
+	"github.com/open-policy-agent/frameworks/constraint/pkg/externaldata"
 	cosign "github.com/sigstore/cosign/cmd/cosign/cli"
 	log "github.com/sirupsen/logrus"
 	k8smnfconfig "github.com/stolostron/integrity-shield/shield/pkg/config"
@@ -37,6 +40,11 @@ const (
 	tlsDir      = `/run/secrets/tls`
 	tlsCertFile = `tls.crt`
 	tlsKeyFile  = `tls.key`
+)
+
+const (
+	apiVersion = "externaldata.gatekeeper.sh/v1alpha1"
+	kind       = "ProviderResponse"
 )
 
 func init() {
@@ -61,26 +69,55 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	log.Info("request received")
+	response := externaldata.ProviderResponse{
+		APIVersion: apiVersion,
+		Kind:       kind,
+		Response: externaldata.Response{
+			Idempotent: true, // mutation requires idempotent results
+		},
+	}
 
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusBadRequest)
+		SendResponse(nil, "only POST is allowed", w)
 		return
 	}
 
 	if r.Header.Get("Content-Type") != "application/json" {
 		w.WriteHeader(http.StatusBadRequest)
+		SendResponse(nil, "only application/json content type is allowed", w)
 		return
 	}
 
 	bufbody := new(bytes.Buffer)
 	_, _ = bufbody.ReadFrom(r.Body)
 	body := bufbody.Bytes()
+
+	// parse request body
+	var providerRequest externaldata.ProviderRequest
+	err := json.Unmarshal(body, &providerRequest)
+	if err != nil {
+		// utils.SendResponse(nil, fmt.Sprintf("unable to unmarshal request body: %v", err), w)
+		SendResponse(nil, fmt.Sprintf("unable to unmarshal request body: %v", err), w)
+		return
+	}
+
+	key := providerRequest.Request.Keys[0]
 	var inputMap map[string]interface{}
 	var request *admission.AdmissionRequest
 	var parameters *k8smnfconfig.ParameterObject
-	err := json.Unmarshal(body, &inputMap)
+
+	if strings.HasSuffix(key, "_systemError") {
+		SendResponse(nil, "testing system error", w)
+		return
+	}
+
+	results := make([]externaldata.Item, 0)
+
+	err = json.Unmarshal([]byte(key), &inputMap)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("unmarshaling input data as map[string]interface{}: %v", err), http.StatusInternalServerError)
+		response.Response.SystemError = fmt.Sprintf("unmarshaling input data as map[string]interface{}: %v", err)
 		return
 	}
 
@@ -99,6 +136,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Infof("request has been parsed successfully, kind: %s, name: %s", request.Kind.Kind, request.Name)
+	subKey := fmt.Sprintf("%s_%s", request.Kind.Kind, request.Name)
 
 	parametersIf, parametersFound := inputMap["parameters"]
 	if !parametersFound {
@@ -116,18 +154,31 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	result := shield.RequestHandler(request, parameters)
-	resp, err := json.Marshal(result)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("marshaling request handler result: %v", err), http.StatusInternalServerError)
-		return
+	if !result.Allow {
+		results = append(results, externaldata.Item{
+			Key:   subKey,
+			Error: result.Message,
+		})
+	} else {
+		results = append(results, externaldata.Item{
+			Key:   subKey,
+			Value: result.Message,
+		})
 	}
 
+	// resp, err := json.Marshal(result)
+	// if err != nil {
+	// 	http.Error(w, fmt.Sprintf("marshaling request handler result: %v", err), http.StatusInternalServerError)
+	// 	return
+	// }
+
 	log.Infof("returning a response, allow: %v", result.Allow)
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(resp); err != nil {
-		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
-		return
-	}
+	// w.WriteHeader(http.StatusOK)
+	// if _, err := w.Write(resp); err != nil {
+	// 	http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+	// 	return
+	// }
+	SendResponse(&results, "", w)
 }
 
 func checkLiveness(w http.ResponseWriter, r *http.Request) {
@@ -165,5 +216,30 @@ func main() {
 
 	if err := serverObj.ListenAndServeTLS("", ""); err != nil {
 		panic(fmt.Sprintf("Fail to run integrity shield api: %v", err))
+	}
+}
+
+// sendResponse sends back the response to Gatekeeper.
+func SendResponse(results *[]externaldata.Item, systemErr string, w http.ResponseWriter) {
+	response := externaldata.ProviderResponse{
+		APIVersion: apiVersion,
+		Kind:       kind,
+		Response: externaldata.Response{
+			Idempotent: true, // mutation requires idempotent results
+		},
+	}
+
+	if results != nil {
+		response.Response.Items = *results
+	} else {
+		response.Response.SystemError = systemErr
+	}
+
+	log.Info("sending response", "response", response)
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Error(err, "unable to encode response")
+		os.Exit(1)
 	}
 }
